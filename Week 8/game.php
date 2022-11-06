23fc510edc8fe4ea38d3c748eebcaa87481aa4a1
nilay4642 <101497968+nilay4642@users.noreply.github.com>
2022-11-06 23:12:47 +0530

    Update game.php

--- Week 8/game.php
+++ Week 8/game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title>NILAY 0b8796fa</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
